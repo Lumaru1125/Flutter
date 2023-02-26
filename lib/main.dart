5e86244705a72eab8c87851695d122a63b7202ef
idl1 <chick46@naver.com>
2023-02-26 16:10:58 +0900

    app bar 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,93 @@
 import 'package:flutter/material.dart';
 
-main(List<String> args) {
-  // 1. runApp
-  runApp(
-      //2. MaterialApp
-      MaterialApp(
-    // home setting widget
-    home: Scaffold(
-        appBar: AppBar(
-          title: const Text("this is title"),
-        ),
-        body: const Text('hello World')),
-  ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Appbar',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Appbar icon menu'),
+        centerTitle: true,
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              print('shopping_cart butten is clicked');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('search butten is clicked');
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+          child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: AssetImage('assets/supper1.JPG'),
+              backgroundColor: Colors.white,
+            ),
+            otherAccountsPictures: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/supper2.jpg'),
+                backgroundColor: Colors.white,
+              ),
+            ],
+            accountName: Text('EJKIM'),
+            accountEmail: Text('chick46@naver.com'),
+            onDetailsPressed: () {
+              print('arros is clicked');
+            },
+            decoration: BoxDecoration(
+                color: Colors.red[200],
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40.0),
+                    bottomRight: Radius.circular(40.0))),
+          ),
+          ListTile(
+            leading: Icon(Icons.home, color: Colors.grey[850]),
+            title: Text('Home'),
+            onTap: () {
+              print('Home is Clicked');
+            },
+            trailing: Icon(Icons.add),
+          ),
+          ListTile(
+            leading: Icon(Icons.settings, color: Colors.grey[850]),
+            title: Text('Settings'),
+            onTap: () {
+              print('Setting is Clicked');
+            },
+            trailing: Icon(Icons.add),
+          ),
+          ListTile(
+            leading: Icon(Icons.question_answer, color: Colors.grey[850]),
+            title: Text('Q&A'),
+            onTap: () {
+              print('Q&A is Clicked');
+            },
+            trailing: Icon(Icons.add),
+          )
+        ],
+      )),
+    );
+  }
 }
