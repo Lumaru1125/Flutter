c1c54322069264e52f9010a9deb03fffd7e3fdc5
yunfengli <liyunfeng_red@hotmail.com>
2023-02-26 18:01:42 +0900

    Add a page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() => runApp(MyApp());
 
@@ -6,22 +6,110 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "First app",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      title: "BBANTO",
+      home: Grade(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("First app")),
-        body: Center(
-          child: Column(
-            children: <Widget>[Text("Hello"), Text("Hello"), Text("hello")],
-          ),
-        ));
+        backgroundColor: Colors.amber[800],
+        appBar: AppBar(
+          title: const Text("BBANTO"),
+          backgroundColor: Colors.amber[700],
+          centerTitle: true,
+          elevation: 0.0,
+        ),
+        body: Padding(
+            padding: const EdgeInsets.fromLTRB(30, 40, 0, 0),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(
+                    child: CircleAvatar(
+                        backgroundImage: AssetImage("assets/unnamed.png"),
+                        radius: 60.0),
+                  ),
+                  Divider(
+                    height: 60.0,
+                    color: Colors.grey[850],
+                    thickness: 0.5,
+                    endIndent: 30.0,
+                  ),
+                  const Text("Name:",
+                      style: TextStyle(
+                        color: Colors.white,
+                        letterSpacing: 2.0,
+                      )),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  const Text('BBANTO',
+                      style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2.0,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold)),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Text("BBANTO POWER LEVEL:",
+                      style: TextStyle(
+                        color: Colors.white,
+                        letterSpacing: 2.0,
+                      )),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  const Text('14',
+                      style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2.0,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold)),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: const <Widget>[
+                      Icon(Icons.check_circle_outline),
+                      SizedBox(width: 10.0),
+                      Text(
+                        "using lightsaber",
+                        style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: const <Widget>[
+                      Icon(Icons.check_circle_outline),
+                      SizedBox(width: 10.0),
+                      Text(
+                        "face hero tattoo",
+                        style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: const <Widget>[
+                      Icon(Icons.check_circle_outline),
+                      SizedBox(width: 10.0),
+                      Text(
+                        "fire flames",
+                        style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                      )
+                    ],
+                  ),
+                  Center(
+                      child: CircleAvatar(
+                    backgroundImage: AssetImage("assets/2x1_SuperMarioHub.jpg"),
+                    radius: 40.0,
+                    backgroundColor: Colors.amber[800],
+                  ))
+                ])));
   }
 }
